af1354f8b31906397fd55baa2d8caf38b1879681
JuliaS0r <Yulyasor0501@yandex.ru>
2023-11-20 04:46:41 +0300

    проверьте дз по методам объектов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author first = new Author("Лев", "Толстой");
+        Author second = new Author("Михаил", "Булгаков");
+
+        Book firstBook = new Book("Война и мир", first, 1869);
+        Book secondBook = new Book("Мастер и Маргарита", second, 1967);
+
+        firstBook.setYear(1234);
+        secondBook.setYear(4321);
+
+        System.out.println("Name: " + firstBook.getNameOfBook() + ". Author: " + first.getFirstName() + " " + first.getLastName() + ". Publication year: " + firstBook.getYear());
+        System.out.println("Name: " + secondBook.getNameOfBook() + ". Author: " + second.getFirstName() + " " + second.getLastName() + ". Publication year: " + secondBook.getYear());
+
+
     }
 }
